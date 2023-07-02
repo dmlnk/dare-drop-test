568bb0874419e3dcf264a89dd0a8725f475aa68e
Daniel Mielnik <d.mielnik@softwarepartner.pl>
2023-07-02 19:50:24 +0200

    fixed cors error on api

--- api/app.js
+++ api/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 const routerV1 = require('./v1/routes/index');
 const database = require('./config/database');
 
@@ -13,7 +13,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/api/v1', routerV1);
 
